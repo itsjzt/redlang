9c360107d6384276a3108d0f98aa64f697226e79
Saurabh codeword <saurabh@codewordstudio.com>
2020-01-02 22:32:57 +0530

    scaning

--- src/scan/scanToken.js
+++ src/scan/scanToken.js
@@ -3,59 +3,78 @@ const tokens = require("./scan/tokens");
 function scanToken(source) {
   const sourceTokens = [];
   let i = 0;
-  do {
-    const c = source[i];
-    switch (c) {
+  while (i < source.length) {
+    const char = source[i];
+    const nextChar = source[i + 1];
+    switch (char) {
       case "(":
         sourceTokens.push(tokens.LEFT_PAREN);
+        i++;
         break;
       case ")":
         sourceTokens.push(tokens.RIGHT_PAREN);
+        i++;
         break;
       case "{":
         sourceTokens.push(tokens.LEFT_BRACE);
+        i++;
         break;
       case "}":
         sourceTokens.push(tokens.RIGHT_BRACE);
+        i++;
         break;
       case ",":
         sourceTokens.push(tokens.COMMA);
+        i++;
         break;
       case ".":
         sourceTokens.push(tokens.DOT);
+        i++;
         break;
       case "-":
         sourceTokens.push(tokens.MINUS);
+        i++;
         break;
       case "+":
         sourceTokens.push(tokens.PLUS);
+        i++;
         break;
       case ";":
         sourceTokens.push(tokens.SEMICOLON);
+        i++;
         break;
       case "*":
         sourceTokens.push(tokens.STAR);
+        i++;
         break;
 
-      // case "!":
-      //   sourceTokens.push(match("=") ? tokens.BANG_EQUAL : tokens.BANG);
-      //   break;
-      // case "=":
-      //   sourceTokens.push(match("=") ? tokens.EQUAL_EQUAL : tokens.EQUAL);
-      //   break;
-      // case "<":
-      //   sourceTokens.push(match("=") ? tokens.LESS_EQUAL : tokens.LESS);
-      //   break;
-      // case ">":
-      //   sourceTokens.push(match("=") ? tokens.GREATER_EQUAL : tokens.GREATER);
-      //   break;
+      case "!":
+        sourceTokens.push(nextChar === "=" ? tokens.BANG_EQUAL : tokens.BANG);
+        break;
+      case "=":
+        sourceTokens.push(nextChar === "=" ? tokens.EQUAL_EQUAL : tokens.EQUAL);
+        break;
+      case "<":
+        sourceTokens.push(nextChar === "=" ? tokens.LESS_EQUAL : tokens.LESS);
+        break;
+      case ">":
+        sourceTokens.push(
+          nextChar === "=" ? tokens.GREATER_EQUAL : tokens.GREATER
+        );
+        break;
       case "/":
-        sourceTokens.push(tokens.SLASH);
+        if (nextChar !== "/") {
+          sourceTokens.push(tokens.SLASH);
+          i++;
+        } else {
+          // skip till the end of line for comment
+          //   i++;
+        }
         break;
       default:
-        sourceTokens.push("Unkonwn token " + String.fromCharCode(c));
+        sourceTokens.push("Unkonwn token " + String.fromCharCode(char));
     }
-  } while (i++ < source.length);
+  }
 
   return sourceTokens;
 }
