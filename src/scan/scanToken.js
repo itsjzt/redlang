3406a0636184aa9e0ab539ee9ccd10eb02e31d18
Saurabh codeword <saurabh@codewordstudio.com>
2020-01-17 17:24:56 +0530

    wow

--- src/scan/scanToken.js
+++ src/scan/scanToken.js
@@ -1,26 +1,53 @@
-const { maths } = require("../scan/maths");
-const { whitespaces } = require("../scan/whitespaces");
-
-function scanToken(source) {
+function scanToken(source = "") {
   const sourceTokens = [];
   let index = 0;
   while (index < source.length) {
     const char = source[index];
-    console.log({ index, length: source.length });
+    const nextChar = source[index + 1];
+    const singleCharSymbols = {
+      "*": "STAR",
+      "+": "PLUS",
+      "-": "DASH",
+      "/": "SLASH",
+      "%": "PERCENT",
+      "<": "ANGLE_LEFT",
+      ">": "ANGLE_RIGHT",
+      "!": "EXCLAMATION",
+      "=": "EQUALS",
+      ",": "COMMA",
+      ";": "SEMICOLON",
+      ".": "DOT",
+      ":": "COLON",
+      '"': "DOUBLE_QUOTE",
+      ")": "PARENTHESIS_CLOSE",
+      "(": "PARENTTHESIS_OPEN",
+      "[": "BRACKET_OPEN",
+      "]": "BRACKET_CLOSE",
+      "{": "BRACES_OPEN",
+      "}": "BRACES_CLOSE"
+    };
 
-    const mathChar = maths({ char, index, source });
-    if (mathChar) {
-      console.log(mathChar);
-      index = mathChar.index;
-      sourceTokens.push(mathChar.token);
-      continue;
-    }
+    const doubleCharSymbol = {
+      "<=": "GREATER_THAN_EQUAL",
+      ">=": "LESS_THAN_EQUAL",
+      "==": "DOUBLE_EQUAL",
+      "!=": "EXCLAMATION_EQUAL"
+    };
 
-    const whitespaceChar = whitespaces({ char, index, source });
-    if (whitespaceChar) {
-      console.log(mathChar);
-      index = whitespaceChar.index;
-      // no token is generated for whitespaces
+    if (char === "/" && nextChar === "/") {
+      // comments before maths because command starts with // and divide symbol is /
+    } else if (char === "/" && nextChar === "*") {
+      // block level comment
+    } else if (Object.keys(doubleCharSymbol).find(s => s === char + nextChar)) {
+      // 2 char symbols before 1 char
+    } else if (Object.keys(singleCharSymbols).find(s => s === char)) {
+      // single char symbols
+    } else if (char.match(/[a-z|A-Z]/)) {
+      // keywords
+    } else if (char.match(/\d/)) {
+      // number
+    } else {
+      // syntax error
     }
   }
 
@@ -28,5 +55,3 @@ function scanToken(source) {
 }
 
 console.log(scanToken("+-*/\t\n"));
-
-module.exports = scanToken;
