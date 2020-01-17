53355515295df415d189c3d9420974105e8469af
Saurabh codeword <saurabh@codewordstudio.com>
2020-01-17 19:16:07 +0530

    tests

--- src/tokenize.js
+++ src/tokenize.js
@@ -1,4 +1,4 @@
-const singleCharSymbols = {
+const singleCharSymbolNames = {
   "*": "STAR",
   "+": "PLUS",
   "-": "DASH",
@@ -21,7 +21,7 @@ const singleCharSymbols = {
   "}": "BRACES_CLOSE"
 };
 
-const doubleCharSymbol = {
+const doubleCharSymbolNames = {
   "<=": "GREATER_THAN_EQUAL",
   ">=": "LESS_THAN_EQUAL",
   "==": "DOUBLE_EQUAL",
@@ -32,26 +32,28 @@ const doubleCharSymbol = {
   "!=": "EXCLAMATION_EQUAL"
 };
 
-function scanToken(source = "") {
-  const sourceTokens = [];
+const singleCharSymbols = Object.keys(singleCharSymbolNames);
+const doubleCharSymbols = Object.keys(doubleCharSymbolNames);
+const whitespaces = [" ", "\t", "\n", "\r\n", "\r"];
+
+function tokenize(source = "") {
+  let tokens = [];
   let index = 0;
   while (index < source.length) {
     const char = source[index];
     const nextChar = source[index + 1];
 
-    if (char === "\n" || char === "\t" || char === " ") {
+    if (whitespaces.includes(char)) {
       // whitespaces
-      index++;
-      continue;
-    } else if (char === "/" && nextChar === "/") {
+      index += 1;
+    } else if (char + nextChar === "//") {
       const skipTill = findIndexAfter({
         after: index,
         string: source,
         toFind: "\n"
       });
       index += skipTill;
-      continue;
-    } else if (char === "/" && nextChar === "*") {
+    } else if (char + nextChar === "/*") {
       // block level comment
       const skipTill = findIndexAfter({
         after: index,
@@ -59,32 +61,49 @@ function scanToken(source = "") {
         toFind: "*/"
       });
       index += skipTill;
-      continue;
-    } else if (Object.keys(doubleCharSymbol).find(s => s === char + nextChar)) {
+    } else if (doubleCharSymbols.includes(char + nextChar)) {
       // 2 char symbols before 1 char
       const symbol = char + nextChar;
-      sourceTokens.push(doubleCharSymbol[symbol]);
-      // cause they are 2 char symbol
-      index += 2;
-    } else if (Object.keys(singleCharSymbols).find(s => s === char)) {
-      // single char symbols
-      sourceTokens.push(singleCharSymbols[char]);
-      index += 1;
+      const tokenName = doubleCharSymbolNames[symbol];
+      tokens.push(tokenName);
+      index += symbol.length;
+    } else if (singleCharSymbols.includes(char)) {
+      // 1 char symbols
+      const tokenName = singleCharSymbolNames[char];
+      tokens.push(tokenName);
+      index += char.length;
     } else if (char.match(/[a-z|A-Z]/)) {
       // keywords
+      index++;
     } else if (char.match(/\d/)) {
       // number
+      index++;
     } else {
       // syntax error
-      throw new Error(`Invalid token, index: ${index} found: ${char}`);
+      throw new Error(`Error: Invalid token, index: ${index} found: ${char}`);
     }
   }
 
-  return sourceTokens;
+  return tokens;
 }
 
 function findIndexAfter({ string = "", after = 0, toFind = "" }) {
-  return string.substring(after).indexOf(toFind);
+  const stringAfter = string.substring(after);
+  const startIndex = stringAfter.indexOf(toFind);
+
+  // handle toFind not present case
+  if (startIndex < 0) {
+    return stringAfter.length;
+  }
+
+  return startIndex + toFind.length;
 }
 
-console.log(scanToken("+-*/\t\n /** jkasdjhkajsd */"));
+module.exports = {
+  tokenize,
+  doubleCharSymbolNames,
+  singleCharSymbolNames,
+  singleCharSymbols,
+  doubleCharSymbols,
+  whitespaces
+};
---
+++ src/tokenize.test.js
@@ -0,0 +1,44 @@
+// @ts-nocheck
+const {
+  tokenize,
+  singleCharSymbolNames,
+  doubleCharSymbolNames,
+  whitespaces
+} = require("./tokenize");
+
+test("whitespaces", () => {
+  for (const whitespace in whitespaces) {
+    expect(tokenize(whitespace)).toStrictEqual([]);
+  }
+  expect(tokenize("\r\n\t")).toStrictEqual([]);
+  expect(tokenize("\r\n\t ")).toStrictEqual([]);
+  expect(tokenize("\r\n\t \n")).toStrictEqual([]);
+});
+
+test("comments", () => {
+  expect(tokenize("// crappy comment\n")).toStrictEqual([]);
+  expect(tokenize("// crappy comment without newline")).toStrictEqual([]);
+  expect(tokenize("/* block of comments */")).toStrictEqual([]);
+  expect(tokenize("/* \n newline \t tabs \r\n \r*/")).toStrictEqual([]);
+});
+
+test("double character symbols", () => {
+  for (const symbol in doubleCharSymbolNames) {
+    expect(tokenize(symbol)).toStrictEqual([doubleCharSymbolNames[symbol]]);
+  }
+});
+
+test("single character symbols", () => {
+  for (const symbol in singleCharSymbolNames) {
+    expect(tokenize(symbol)).toStrictEqual([singleCharSymbolNames[symbol]]);
+  }
+});
+
+test("invalid characters", () => {
+  const invalidCharacters = "@#$^&|?~`".split("");
+  // TODO:
+  for (const char of invalidCharacters) {
+    console.log({ char });
+    expect(tokenize(char)).toThrowError();
+  }
+});
