01d5624dd8c358f2e211dc4f7a82b87a476c579b
Saurabh Sharma <jztsaurabh@gmail.com>
2024-05-05 10:45:47 +0530

    wip

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ learning to make interpreted programming language,
 - use special function to parse keywords of language
 - keep an index of line column and line number in scanLine
 - use an error reporting interface
+
+## Things needed to do now
+
+- Array primitives
+- HashMap primitives
+- Class and OOP support
+- import/export support
+- Math lib
+- Fs lib
+- Os lib
+- UDP/TCP/HTTP lib
+- Buffer
+
+<!-- can be done later -->
+
+- Crypto
+- IO
+- Time/Date
+- timer
+- zlib
--- src/modules/resolver/resolveVariables.ts
+++ src/modules/resolver/resolveVariables.ts
@@ -1,88 +1,104 @@
-// import { throwParsingError } from "../../throwError";
-// import { VariableExpr } from "../parser/expr";
-// import { BlockStmt, Stmt, VarStmt } from "../parser/stmt";
-// import { Token } from "../scanner/token";
+import { throwParsingError } from "../../throwError";
+import { AssignExpr, Expr, VariableExpr } from "../parser/expr";
+import { BlockStmt, Stmt, VarStmt } from "../parser/stmt";
+import { Token } from "../scanner/token";
 
-// let scopes: Array<Map<string, any>> = [];
+let scopes: Array<Map<string, any>> = [];
 
-// export function resolver(statements: Stmt[]) {
-//   return visitStatements(statements);
-// }
+export function resolver(statements: Stmt[]) {
+  return visitStatements(statements);
+}
 
-// function visitStatements(stmts: Stmt[]) {
-//   for (let statement of stmts) {
-//     visitStatement(statement);
-//   }
-// }
+function visitStatements(stmts: Stmt[]) {
+  for (let statement of stmts) {
+    visitStatement(statement);
+  }
+}
 
-// function visitStatement(stmt: Stmt) {
-//   switch (stmt.type) {
-//     case "BlockStmt":
-//       visitBlockStatement(stmt);
-//       break;
-//     case "VarStmt":
-//       visitVariableDeclaration(stmt);
-//   }
-// }
+function visitStatement(stmt: Stmt) {
+  switch (stmt.type) {
+    case "BlockStmt":
+      visitBlockStatement(stmt);
+      break;
+    case "VarStmt":
+      visitVariableDeclaration(stmt);
+  }
+}
 
-// function visitVariableDeclaration(stmt: VarStmt) {
-//   declare(stmt.name);
-//   if (stmt.initializer !== null) {
-//     resolve(stmt.initializer);
-//   }
+function visitExpression(expr: Expr) {}
 
-//   define(stmt.name);
-//   return null;
-// }
+function visitVariableDeclaration(stmt: VarStmt) {
+  declare(stmt.name);
+  if (stmt.initializer !== null) {
+    visitExpression(stmt.initializer);
+  }
 
-// function declare(name: Token) {
-//   if (scopes.length === 0) {
-//     return;
-//   }
+  define(stmt.name);
+  return null;
+}
 
-//   let scope = scopes.at(-1);
-//   if (scope) {
-//     scope.set(name.lexeme!, false);
-//   }
-// }
+function declare(name: Token) {
+  if (scopes.length === 0) {
+    return;
+  }
 
-// function define(name: Token) {
-//   if (scopes.length === 0) {
-//     return;
-//   }
+  let scope = scopes.at(-1);
+  if (scope) {
+    scope.set(name.lexeme!, false);
+  }
+}
 
-//   let scope = scopes.at(-1);
-//   if (scope) {
-//     scope.set(name.lexeme!, true);
-//   }
-// }
+function define(name: Token) {
+  if (scopes.length === 0) {
+    return;
+  }
 
-// function visitVariableExpr(expr: VariableExpr) {
-//   if (scopes.length !== 0 && scopes.at(-1)!.get(expr.name.lexeme!) === false) {
-//     throw throwParsingError(
-//       expr.name,
-//       "Can't read local variable in its own initializer"
-//     );
-//   }
+  let scope = scopes.at(-1);
+  if (scope) {
+    scope.set(name.lexeme!, true);
+  }
+}
 
-//   resolveLocal(expr, expr.name);
-//   return null;
-// }
+function visitVariableExpr(expr: VariableExpr) {
+  if (scopes.length !== 0 && scopes.at(-1)!.get(expr.name.lexeme!) === false) {
+    throw throwParsingError(
+      expr.name,
+      "Can't read local variable in its own initializer"
+    );
+  }
 
-// function resolveLocal() {}
+  resolveLocal(expr, expr.name);
+  return null;
+}
 
-// function visitBlockStatement(stmt: BlockStmt) {
-//   beginScope();
-//   visitStatements(stmt.statements);
-//   endScope();
+function resolveLocal(expr: VariableExpr, name: Token) {
+  for (let i = scopes.length - 1; i >= 0; i--) {
+    if (scopes[i].has(name.lexeme!)) {
+      // resolve
+      // save the variable resolution
+      return;
+    }
+  }
+}
 
-//   return null;
-// }
+function visitAssignExpr(expr: AssignExpr) {
+  visitExpression(expr.value);
+  resolveLocal(expr, expr.name);
+  return;
+}
 
-// function beginScope() {
-//   scopes.push(new Map<string, any>());
-// }
+function visitBlockStatement(stmt: BlockStmt) {
+  beginScope();
+  visitStatements(stmt.statements);
+  endScope();
 
-// function endScope() {
-//   scopes.pop();
-// }
+  return null;
+}
+
+function beginScope() {
+  scopes.push(new Map<string, any>());
+}
+
+function endScope() {
+  scopes.pop();
+}
